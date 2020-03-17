d6d94ea3b4b5388d8aea61aab644b828694fcc21
Omer Geva <ogeva97@gmail.com>
2020-03-17 21:13:42 +0200

    Updates

---
+++ precache-manifest.e59e7fc129b32d99b8657f96aad6df87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae82dc461ec937b0c279b4348c71a01a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4c60106c8f6fdc5d4ed7",
+    "url": "/monsters-rolodex/static/css/main.9c0068cb.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c60106c8f6fdc5d4ed7",
+    "url": "/monsters-rolodex/static/js/main.f710523b.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
